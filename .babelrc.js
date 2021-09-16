4742de35bafee364d5bc81dd6b11cd14a83d6191
19h47 <jeremylevron@19h47.fr>
2021-09-16 17:22:36 +0200

    :arrow_up: Upgrade dependencies

--- .babelrc.js
+++ .babelrc.js
@@ -2,16 +2,7 @@ const presets = [
 	[
 		'@babel/preset-env',
 		{
-			modules: false,
-			targets: {
-				edge: '17',
-				firefox: '60',
-				chrome: '67',
-				safari: '11.1',
-				ie: '11',
-			},
-			corejs: 3,
-			useBuiltIns: 'usage',
+			targets: '>0.25%',
 		},
 	],
 ];
